d7f2fe7d3846bd4fddf2e9a83eb45052ac6d1363
Andrea Tinelli <tinelliandrea2@outlook.it>
2021-10-20 11:43:20 +0200

    Pulizia repository

--- ocaml/overview_&_recursion/pattern_matching.ml
+++
@@ -1,9 +0,0 @@
-(* Funzione per invertire il valore di un booleano tramite pattern matching *)
-let invert x =
-    match x with
-        | true -> false
-        | false -> true;;
-
-(* Versione compatta della medesima funzione con pattern matching *)
-let invert´ = function 
-    true -> false | false -> true;;
