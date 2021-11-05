249194f3219ab68e58061b7683c41db2e37ba77c
Andrea Tinelli <tinelliandrea2@outlook.it>
2021-11-05 11:33:37 +0100

    Rimosse consegne

--- ocaml/ex1.ml
+++ ocaml/ex1.ml
@@ -1,18 +1,3 @@
-(*
-Exercise 1: A few of Chemistry.
-
-Put into a list, called alkaline_earth_metals, the atomic numbers of the six alkaline earth metals: 
-beryllium (4), magnesium (12), calcium (20), strontium (38), barium (56), and radium (88). 
-Then
-  - Write a function that returns the highest atomic number in alkaline_earth_metals.
-  - Write a function that sorts alkaline_earth_metals in ascending order (from the lightest to the heaviest).
-
-Put into a second list, called noble_gases, the noble gases: helium (2), neon (10),
-argon (18), krypton (36), xenon (54), and radon (86). 
-Then
-  - Write a function (or a group of functions) that merges the two lists and print the result as couples (name, atomic number) sorted in ascending order on the element names.
-*)
-
 let alkaline_earth_metals = [56; 4; 12; 88; 38; 20] and noble_gases = [2; 10; 18; 36; 54; 86];;
 
 (* finds the biggest element inside the list *)
--- ocaml/ex4.ml
+++ ocaml/ex4.ml
@@ -1,13 +1,3 @@
-(*
-Exercise 4: Frequencies.
-
-Let's write a function (or a pool of functions) that given a quite large text (over than 2000 words) counts how frequent each word occurs in the text.
-
-The text is read from a file (look at the pervasive module in the manual) and it is a real text with punctuation (i.e., commas, semicolons, ...) that should be counted.
-
-Note that words with different case should be considered the same.
-*)
-
 exception EmptyString
 
 type word_frequency = { word: string; mutable frequency: int }
