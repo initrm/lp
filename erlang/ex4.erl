dd6ae86be743910e48e2708c888551ac7e994c2f
Andrea Tinelli <tinelliandrea2@outlook.it>
2021-11-24 11:07:08 +0100

    Aggiunti commenti

--- erlang/ex4.erl
+++ erlang/ex4.erl
@@ -1,12 +1,16 @@
 -module(ex4).
 -export([start/0, print/1, stop/0]).
 
+% starts the server registering the name of the process with the atom 'echo'
 start() -> register(echo, spawn(fun() -> loop() end)).
 
+% prints the submitted argument on the server
 print(Term) -> echo ! {print, Term}, ok.
 
+% stops the server
 stop() -> echo ! stop, ok.
 
+% server behaviour
 loop() ->
   receive
     {print, M} -> io:format("~p~n", [M]), loop();
